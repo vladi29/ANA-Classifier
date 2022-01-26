0d4d322ef86deaaa7af8284c61bf1b3d05e3b4fb
vladi29 <42014792+vladi29@users.noreply.github.com>
2022-01-26 19:29:05 -0400

    Todo parece funcionar bien

--- main.py
+++ main.py
@@ -14,8 +14,17 @@ import matplotlib.pyplot as plt
 import random
 import pandas as pd
 import os
+from preprocessing import preprocessing_training as ppt
 
 # Paths
 
-images_path = 'C:/Users/Usuario/OneDrive/Escritorio/Tesis/Datasets/AIDA_HEp-2_DB_publico/aida_project_database'
 labels_path = 'C:/Users/Usuario/OneDrive/Escritorio/Tesis/Datasets/AIDA_HEp-2_DB_publico/AIDA_HEp-2_GroundTruth.xlsx'
+images_path = 'C:/Users/Usuario/OneDrive/Escritorio/Tesis/Datasets/AIDA_HEp-2_DB_publico/aida_project_database'
+
+labels, images, names = ppt(labels_path, images_path)
+
+random_number = random.randint(0, 2079)
+print("Name: ", names[random_number])
+print("Label: ", labels[random_number])
+plt.imshow(images[random_number])
+plt.show()
--- preprocessing.py
+++ preprocessing.py
@@ -20,14 +20,15 @@ def preprocessing_training(Labels_path, Images_path):
     images_list = []
     for image in images:
         print(image)
-        image_path = images_path + '/' + image
+        image_path = Images_path + '/' + image
         image = cv2.imread(image_path)
         image = (image/255).astype('float32')
         image = cv2.resize(image, (100, 75))
         images_list.append(image)
 
-    return labels_list, images_list
+    return labels_list, images_list, images
 
+    #-----------Obtener la cantidad de labels-----------
     # aux = ''
     # patterns = {}
     # for pattern in labels_list:
