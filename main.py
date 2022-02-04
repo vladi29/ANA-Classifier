bd24f3bbff7c714fe4a8ae6d4c37e7b6bd04f900
vladi29 <42014792+vladi29@users.noreply.github.com>
2022-02-04 11:33:11 -0400

    Se extraen correctamente las imagenes y las etiquetas en el main.py. Parece que las imagenes no concuerdan con sus respectivas etiquetas

--- main.py
+++ main.py
@@ -20,47 +20,42 @@ import os
 labels_path = 'labels.csv'
 images_path = 'images.npz'
 
-labels = np.load(labels_path)
+labels = pd.read_csv(labels_path)
+labels = labels.values.tolist()
+
 images = np.load(images_path)
+images = images['arr_0']
 
 images_list = []
 labels_list = []
-names_list = []
 
 A = random.randint(0, 2079)
 images_list.append(images[A])
-names_list.append(names[A])
 labels_list.append(labels[A])
 
 B = random.randint(0, 2079)
 images_list.append(images[B])
-names_list.append(names[B])
 labels_list.append(labels[B])
 
 C = random.randint(0, 2079)
 images_list.append(images[C])
-names_list.append(names[C])
 labels_list.append(labels[C])
 
 D = random.randint(0, 2079)
 images_list.append(images[D])
-names_list.append(names[D])
 labels_list.append(labels[D])
 
 E = random.randint(0, 2079)
 images_list.append(images[E])
-names_list.append(names[E])
 labels_list.append(labels[E])
 
 F = random.randint(0, 2079)
 images_list.append(images[F])
-names_list.append(names[F])
 labels_list.append(labels[F])
 
 for i in range(0,6):
     plt.subplot(2,3, i+1)
     imagen = images_list[i]
-    print("Name: ", names_list[i])
     print("Label: ", labels_list[i])
     plt.imshow(imagen)
 plt.show()
