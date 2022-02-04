b6d9aa7e9b98a3b2c11c9fe6330f51408236c3dd
vladi29 <42014792+vladi29@users.noreply.github.com>
2022-02-04 11:47:07 -0400

    Se simplifico el codigo y se agrego la opcion de leer e imprimir los nombres de las imagenes para ver si las etiquetas y las imagenes corresponde unas con otras

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ import os
 
 labels_path = 'labels.csv'
 images_path = 'images.npz'
+names_path = 'names.csv'
 
 labels = pd.read_csv(labels_path)
 labels = labels.values.tolist()
@@ -26,36 +27,23 @@ labels = labels.values.tolist()
 images = np.load(images_path)
 images = images['arr_0']
 
+names = pd.read_csv(names_path)
+names = names.values.tolist()
+
 images_list = []
 labels_list = []
+names_list = []
 
-A = random.randint(0, 2079)
-images_list.append(images[A])
-labels_list.append(labels[A])
-
-B = random.randint(0, 2079)
-images_list.append(images[B])
-labels_list.append(labels[B])
-
-C = random.randint(0, 2079)
-images_list.append(images[C])
-labels_list.append(labels[C])
-
-D = random.randint(0, 2079)
-images_list.append(images[D])
-labels_list.append(labels[D])
-
-E = random.randint(0, 2079)
-images_list.append(images[E])
-labels_list.append(labels[E])
-
-F = random.randint(0, 2079)
-images_list.append(images[F])
-labels_list.append(labels[F])
+for i in range(0, 6):
+    A = random.randint(0, 2079)
+    images_list.append(images[A])
+    labels_list.append(labels[A])
+    names_list.appenf(names[A])
 
 for i in range(0,6):
     plt.subplot(2,3, i+1)
     imagen = images_list[i]
     print("Label: ", labels_list[i])
+    print("Image name: ", names_list[i])
     plt.imshow(imagen)
 plt.show()
