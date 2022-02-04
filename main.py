015d8868f98e732f64417ad13ae1ca1e6cf2b90a
vladi29 <42014792+vladi29@users.noreply.github.com>
2022-02-04 10:43:09 -0400

    Corredigo el gitignore

--- main.py
+++ main.py
@@ -18,49 +18,49 @@ from preprocessing import preprocessing_training as ppt
 
 # Paths
 
-labels_path = 'C:/Users/Usuario/OneDrive/Escritorio/Tesis/Datasets/AIDA_HEp-2_DB_publico/AIDA_HEp-2_GroundTruth.xlsx'
-images_path = 'C:/Users/Usuario/OneDrive/Escritorio/Tesis/Datasets/AIDA_HEp-2_DB_publico/aida_project_database'
+labels_path = 'labels.csv'
+images_path = 'images.npz'
 
-labels, images, names = ppt(labels_path, images_path)
+#labels, images, names = ppt(labels_path, images_path)
 
-images_list = []
-labels_list = []
-names_list = []
+# images_list = []
+# labels_list = []
+# names_list = []
 
-A = random.randint(0, 2079)
-images_list.append(images[A])
-names_list.append(names[A])
-labels_list.append(labels[A])
+# A = random.randint(0, 2079)
+# images_list.append(images[A])
+# names_list.append(names[A])
+# labels_list.append(labels[A])
 
-B = random.randint(0, 2079)
-images_list.append(images[B])
-names_list.append(names[B])
-labels_list.append(labels[B])
+# B = random.randint(0, 2079)
+# images_list.append(images[B])
+# names_list.append(names[B])
+# labels_list.append(labels[B])
 
-C = random.randint(0, 2079)
-images_list.append(images[C])
-names_list.append(names[C])
-labels_list.append(labels[C])
+# C = random.randint(0, 2079)
+# images_list.append(images[C])
+# names_list.append(names[C])
+# labels_list.append(labels[C])
 
-D = random.randint(0, 2079)
-images_list.append(images[D])
-names_list.append(names[D])
-labels_list.append(labels[D])
+# D = random.randint(0, 2079)
+# images_list.append(images[D])
+# names_list.append(names[D])
+# labels_list.append(labels[D])
 
-E = random.randint(0, 2079)
-images_list.append(images[E])
-names_list.append(names[E])
-labels_list.append(labels[E])
+# E = random.randint(0, 2079)
+# images_list.append(images[E])
+# names_list.append(names[E])
+# labels_list.append(labels[E])
 
-F = random.randint(0, 2079)
-images_list.append(images[F])
-names_list.append(names[F])
-labels_list.append(labels[F])
+# F = random.randint(0, 2079)
+# images_list.append(images[F])
+# names_list.append(names[F])
+# labels_list.append(labels[F])
 
-for i in range(0,6):
-    plt.subplot(2,3, i+1)
-    imagen = images_list[i]
-    print("Name: ", names_list[i])
-    print("Label: ", labels_list[i])
-    plt.imshow(imagen)
-plt.show()
+# for i in range(0,6):
+#     plt.subplot(2,3, i+1)
+#     imagen = images_list[i]
+#     print("Name: ", names_list[i])
+#     print("Label: ", labels_list[i])
+#     plt.imshow(imagen)
+# plt.show()
