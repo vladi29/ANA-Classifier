a7488def5bbc51c0db9438a1923bfac545938783
vladi29 <42014792+vladi29@users.noreply.github.com>
2022-02-04 10:40:28 -0400

    Modificado preprocessing.py para comprimir y almacenar las imagenes en un archivo images.npz para ser usado en main.py y acelerar el proceso de entrenamiento

--- preprocessing.py
+++ preprocessing.py
@@ -4,38 +4,49 @@
 
 # Preprocesamiento de las imagenes
 
-import matplotlib.pyplot as plt
 import pandas as pd
-import os
-from natsort import os_sorted
+import time
 import cv2
 import random
+import numpy as np
 
-def preprocessing_training(Labels_path, Images_path):
+labels_path = 'C:/Users/Usuario/OneDrive/Escritorio/Tesis/Datasets/AIDA_HEp-2_DB_publico/AIDA_HEp-2_GroundTruth.xlsx'
+images_path = 'C:/Users/Usuario/OneDrive/Escritorio/Tesis/Datasets/AIDA_HEp-2_DB_publico/aida_project_database'
 
-    data = pd.read_excel(Labels_path)
-    labels_list = data['StainingPatterns'].values.tolist()
-    images = data['ImageFileName'].values.tolist()
+df = pd.read_excel(labels_path, header = 0)
+shuffled_df = df.sample(frac = 1)
+shuffled_df.reset_index(drop = True, inplace = True)
+shuffled_df.to_excel('shuffled_images.xlsx')
 
-    images_list = []
-    for image in images:
-        print(image)
-        image_path = Images_path + '/' + image
-        image = cv2.imread(image_path)
-        image = (image/255).astype('float32')
-        image = cv2.resize(image, (100, 75))
-        images_list.append(image)
+data = pd.read_excel('shuffled_images.xlsx')
+labels_list = data['StainingPatterns'].values.tolist()
+images = data['ImageFileName'].values.tolist()
 
-    #-----------Obtener la cantidad de labels-----------
-    aux = ''
-    patterns = {}
-    for pattern in labels_list:
-        if pattern != aux:
-            patterns[pattern] = labels_list.count(pattern)
-            aux = pattern
-        else:
-            continue
-    #print(patterns)
-    print('Numero de patrones en el dataset: ', len(patterns))
+start_time = time.time()
+images_list = []
+for image in images:
+    print(image)
+    image_path = images_path + '/' + image
+    image = cv2.imread(image_path)
+    image = (image/255).astype('float32')
+    image = cv2.resize(image, (100, 75))
+    images_list.append(image)
+final_time = time.time() - start_time
+print('Tiempo de preprocesado de las images:', final_time/60)
 
-    return labels_list, images_list, images
+labels = np.asarray(labels_list)
+np.savetxt('labels.csv', labels, fmt ='%s', delimiter = ',')
+images_compressed = np.asarray(images_list)
+np.savez_compressed('images.npz', images_compressed)
+
+#-----------Obtener la cantidad de labels-----------
+# aux = ''
+# patterns = {}
+# for pattern in labels_list:
+#     if pattern != aux:
+#         patterns[pattern] = labels_list.count(pattern)
+#         aux = pattern
+#     else:
+#         continue
+# #print(patterns)
+# print('Numero de patrones en el dataset: ', len(patterns))
